6e2f5c473d541f639ee1af6ea1904c336c037afd
Alex Racinski <aracinski2@gmail.com>
2020-11-13 17:16:05 -0600

    add index, show, create, update, delete actions to controller

--- app/controllers/api/pizzas_controller.rb
+++ app/controllers/api/pizzas_controller.rb
@@ -1,2 +1,38 @@
 class Api::PizzasController < ApplicationController
+  
+  def index
+    @pizzas = Pizza.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @pizza = Pizza.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @pizza = Pizza.new(style: params[:style],
+    size: params[:size],
+    topping: params[:topping],
+    price: params[:price])
+    @pizza.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @pizza = Pizza.find_by(id: params[:id])
+    @pizza.style = params[:style]
+    @pizza.size = params[:size]
+    @pizza.topping = params[:topping]
+    @pizza.price = params[:price]
+    @pizza.save
+    render 'show.json.jb'
+  end
+
+  def delete
+    @pizza = Pizza.find_by(id: params[:id])
+    @pizza.destroy
+    render json: {message: "Pizza deleted, how could you?"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/pizzas" => "pizzas#index"
+    get "/pizzas/:id" => "pizzas#show"
+    post "/pizzas" => "pizzas#create"
+    patch "/pizzas/:id" => "pizzas#update"
+    delete "/pizzas/:id" => "pizzas#delete"
+  end
+  
 end
